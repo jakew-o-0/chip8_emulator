e14bb838a668b03599213954f29a08194ae68218
jakew-o-0 <jakewbailey007@gmail.com>
2022-12-14 17:40:50 +0000

    refactor 2

--- chip8.cpp
+++ chip8.cpp
@@ -1,63 +1,10 @@
 #include<fstream>
-#include <iostream>
-#include <sys/types.h>
+#include<iostream>
+#include<sys/types.h>
 #include<vector>
 #include<string>
 
-
-class Chip8_CPU
-{
-private:
-    unsigned char RAM[4096];       // 4KB memory
-    unsigned char REG[16];         // general purpose registers 0-F, F is used as a carry flag
-    uint16_t STACK[16];     // calling and returning from subroutines
-    uint16_t SP;            // stack pointer
-    uint16_t I;             // index register, holds a location in memory, often used for drawing to SCREEN
-    uint16_t PC;            // program counter, holds next address to be interpreted
-    unsigned char DELAY_TIMER;     // will count douwn while its value is greater than 0
-    unsigned char SOUND_TIMER;     // will count down, making a noise, while value is greater than 0
-    unsigned char SCREEN[64 * 32]; // 64x23 pixel screen, 0x00 -> off and 0xFF -> on
-
-    /*========= Memory map =========
-        - 0x000 -> 0x1FF    chip8 interpreter( only necicary for character font )
-        - 0x050 -> 0x0A0    character fonts location
-        - 0x200 -> 0xFFF    program memory and work ram
-    ================================*/
-
-
-    uint16_t opcode;        // holds curent instruction to be decoded
-    unsigned char regX;
-    unsigned char regY;
-    unsigned char cond_value;
-
-
-public:
-    void initialise();
-    void load_game(std::string path);
-    void fetch();
-    void decode();
-
-private:
-    void zero_group();
-    void jump_address();
-    void call_subroutine();
-    void VX_equal_NN();
-    void VX_notEqual_NN();
-    void VX_equal_VY();
-    void set_VX_to_NN();
-    void add_NN_to_VX();
-    void math_logic_group();
-    void VX_notEqual_VY();
-    void set_I_to_NNN();
-    void jump_to_NNN_plus_V0();
-    void VX_to_RAND_AND_NN();
-    void draw_to_screen();
-    void E_group();
-    void F_group();
-};
-
-
-
+#include<chip8.h>
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -65,7 +12,7 @@ private:
 ////////////////////////////////////////////////////////////////////////
 
 
-void Chip8_CPU::initialise(){
+void Chip8_CPU::initialise(){   // empties memory and loads the fontset into ram
     PC = 0x0200;
     SP = 0;
     I = 0;
@@ -118,7 +65,7 @@ void Chip8_CPU::initialise(){
 ///////////////////////////////////////////////////////////
 
 
-void Chip8_CPU::load_game(std::string path){
+void Chip8_CPU::load_game(std::string path){    // reads binary file, loads it into ram to be executed
     std::ifstream file;
     file.open(path, std::ios::binary | std::ios::in);
 
@@ -171,11 +118,14 @@ void Chip8_CPU::fetch() {
 ///////////////////////////////////////////////////////////
 
 
+//using the opcode fetched from the fetch function,the binary is decoded
+// layed out in two bytes, each nibble is, possibly a diffrent part of an instruction
+// the first nibble indicates what type of instruction it is
 void Chip8_CPU::decode() {
-    uint16_t op_type = opcode & 0xF000;
-    regX = ((opcode & 0x0F00) >> (2 * 8)) & 0xFF;
-    regY = ((opcode & 0x00F0) >> 8) & 0xFF;
-    cond_value = (opcode & 0x00FF) & 0xFF;
+    uint16_t op_type = opcode & 0xF000;             // isolates most significant nibble, indicates instrucion type
+    regX = ((opcode & 0x0F00) >> (2 * 8)) & 0xFF;   // isolates third nibble, often used to point to a register
+    regY = ((opcode & 0x00F0) >> 8) & 0xFF;         // isolates second nibble, often used to point to a register 
+    cond_value = (opcode & 0x00FF) & 0xFF;          // isolates the two least significant nibbles, often used as a condition or a value
 
     switch (op_type){
 
@@ -183,8 +133,8 @@ void Chip8_CPU::decode() {
             zero_group();
             break;
 
-        case 0x1000:
-            jump_address();
+        case 0x1000:                // 0x1NNN jumps to the address given 
+            PC = opcode & 0x0FFF;   // pc set to the given address
             break;
 
         case 0x2000:
@@ -203,12 +153,12 @@ void Chip8_CPU::decode() {
             VX_equal_VY();
             break;
 
-        case 0x6000:
-            VX_equal_NN();
+        case 0x6000:    // 0x6XNN sets the value of register X to NN
+            REG[regX] = cond_value;     
             break;
 
-        case 0x7000:
-            add_NN_to_VX();
+        case 0x7000:    // 0x7XNN adds byte NN to register VX, without carry flag
+            REG[regX] += cond_value;
             break;
 
         case 0x8000:
@@ -219,16 +169,16 @@ void Chip8_CPU::decode() {
             VX_notEqual_VY();
             break;
 
-        case 0xA000:
-            set_I_to_NNN();
+        case 0xA000:    // 0xANNN asigns the index register to the given address
+            I = opcode & 0x0FFF;
             break;
 
-        case 0xB000:
-            jump_to_NNN_plus_V0();
+        case 0xB000:    // 0xBNNN jumps to location NNN plus register 0
+            PC = REG[0] + (opcode & 0x0FFF);
             break;
 
-        case 0xC000:
-            VX_to_RAND_AND_NN();
+        case 0xC000:   // 0xCXNN assigns register x to a random number AND a one byte value given 
+            REG[regX] = (std::rand() % 255) & cond_value;
             break;
 
         case 0xD000:
@@ -266,16 +216,6 @@ void Chip8_CPU::zero_group(){
         PC = STACK[SP];
         STACK[SP] = 0x0000;
     }
-}
-
-
-////////////////////////////////////////////////////////////
-
-
-// 0x1NNN jumps to the address given
-// pc set to the given address
-void Chip8_CPU::jump_address(){
-    PC = opcode & 0x0FFF;
 }
 
 
@@ -328,24 +268,6 @@ void Chip8_CPU::VX_equal_VY(){
     }
 }
 
-///////////////////////////////////////////////////////////
-
-
-// 0x6XNN sets the value of register X to NN
-void Chip8_CPU::set_VX_to_NN(){
-    REG[regX] = cond_value;
-}
-
-
-//////////////////////////////////////////////////////////
-
-
-// 0x7XNN adds byte NN to register VX
-// without carry flag
-void Chip8_CPU::add_NN_to_VX(){
-    REG[regX] += cond_value;
-}
-
 
 /////////////////////////////////////////////////////////
 
@@ -354,100 +276,84 @@ void Chip8_CPU::math_logic_group(){
     u_char op_subType = (opcode & 0x000F) & 0xFF;
     u_char prev_regX = REG[regX];
 
-    switch (op_subType)
-    {
-    // assigns regX to value of regY
-    case 0x00:
-        REG[regX] = REG[regY];
-        break;
+    switch (op_subType){
+
+        case 0x00:  // assigns regX to value of regY
+            REG[regX] = REG[regY];
+            break;
 
 
 
-    // assigns regx to value of regy OR regx
-    case 0x01:
-        REG[regX] |= REG[regY];
-        break;
+        case 0x01:  // assigns regx to value of regy OR regx
+            REG[regX] |= REG[regY];
+            break;
+
+
+        
+        case 0x02:  // assigns regx to value of regy AND regx
+            REG[regX] &= REG[regY];
+            break;
 
 
 
-    // assigns regx to value of regy AND regx
-    case 0x02:
-        REG[regX] &= REG[regY];
-        break;
+        case 0x03:  // assigns regx to value of regy XOR regx
+            REG[regX] ^= REG[regY];
+            break;
+
+
+        
+        case 0x04:  // adds regY to regX with carry flag
+            prev_regX = REG[regX];
+            REG[regX] += REG[regY];
+            REG[0xF] = 0;
+
+            if (prev_regX > REG[regX]) {    // if the previous value is bigger than the current,
+                REG[0xF] = 1;               // the integer has wrapped and a carry has taken place
+            }
+            break;
 
 
 
-    // assigns regx to value of regy XOR regx
-    case 0x03:
-        REG[regX] ^= REG[regY];
-        break;
-
-
-
-    // adds regY to regX with carry flag
-    case 0x04:
-        prev_regX = REG[regX];
-        REG[regX] += REG[regY];
-        REG[0xF] = 0;
-
-        // if the previous value is bigger than the current,
-        // the integer has wrapped and a carry has taken place
-        if (prev_regX > REG[regX]) {
+        case 0x05:  // subtracts regY to regX with borrow flag
+            prev_regX = REG[regX];
+            REG[regX] -= REG[regY];
             REG[0xF] = 1;
-        }
-        break;
+
+            if (prev_regX < REG[regX]) {    // if the previous value is smaller than the current,
+                REG[0xF] = 0;               // the integer has wrapped and a borrow has taken place
+            }
+            break;
 
 
 
-    // subtracts regY to regX with borrow flag
-    case 0x05:
-        prev_regX = REG[regX];
-        REG[regX] -= REG[regY];
-        REG[0xF] = 1;
-
-        // if the previous value is smaller than the current,
-        // the integer has wrapped and a borrow has taken place
-        if (prev_regX < REG[regX]) {
-            REG[0xF] = 0;
-        }
-        break;
+        case 0x06:
+            REG[0xF] = (REG[regX] & 1); // stores the smallest bit in register F
+            REG[regX] >>= 1;            // then shifts regx right 1
+            break;
 
 
 
-    // stores the smallest bit in register F
-    // then shifts regx right 1
-    case 0x06:
-        REG[0xF] = (REG[regX] & 1);
-        REG[regX] >>= 1;
-        break;
+        case 0x07:  // sets register x to register y - x, with borrow flag
+            prev_regX = REG[regX];
+            REG[regX] = REG[regY] - REG[regX];
+            REG[0xF] = 1;
+
+            if (prev_regX < REG[regX]) {
+                REG[0xF] = 0;
+            }
+            break;
+
+
+        
+        case 0x0E:
+            REG[0xF] = (REG[regX] & (1 << 8));  // stores the largest bit in register F
+            REG[regX] <<= 1;                    // then shifts regx left 1
+            break;
 
 
 
-    // sets register x to register y - x
-    // with borrow flag
-    case 0x07:
-        prev_regX = REG[regX];
-        REG[regX] = REG[regY] - REG[regX];
-        REG[0xF] = 1;
-
-        if (prev_regX < REG[regX]) {
-            REG[0xF] = 0;
-        }
-        break;
-
-
-
-    // stores the largest bit in register F
-    // then shifts regx left 1
-    case 0x0E:
-        REG[0xF] = (REG[regX] & (1 << 8));
-        REG[regX] <<= 1;
-        break;
-
-
-
-    default:
-        break;
+        default:
+            break;
     }
 }
 
@@ -466,33 +372,6 @@ void Chip8_CPU::VX_notEqual_VY(){
 //////////////////////////////////////////////////////////////////
 
 
-// 0xANNN asigns the index register to the given address
-void Chip8_CPU::set_I_to_NNN(){
-    I = opcode & 0x0FFF;
-}
-
-
-//////////////////////////////////////////////////////////////////
-
-
-// 0xBNNN jumps to location NNN plus register 0
-void Chip8_CPU::jump_to_NNN_plus_V0(){
-    PC = REG[0] + (opcode & 0x0FFF);
-}
-
-
-//////////////////////////////////////////////////////////////////
-
-
-// 0xCXNN assigns register x to a random number AND a one byte value given
-void Chip8_CPU::VX_to_RAND_AND_NN(){
-    REG[regX] = (std::rand() % 255) & cond_value;
-}
-
-
-//////////////////////////////////////////////////////////////////
-
-
 void draw_to_screen(){
 
 }
@@ -503,38 +382,39 @@ void draw_to_screen(){
 
 void E_group();
 void Chip8_CPU::F_group(){
-    unsigned char op_subType = (opcode & 0x00FF) & 0xFF;
-    switch (op_subType)
-    {
+    u_char op_subType = (opcode & 0x00FF) & 0xFF;
+
+    switch (op_subType){
+
     // 0xFX07 assigns register x to the value of the delay timer
-    case 0x07:
-        REG[regX] = DELAY_TIMER;
-        break;
+        case 0x07:
+            REG[regX] = DELAY_TIMER;
+            break;
 
-    case 0x0A:
-        //waits for key input
-        break;
-        
-    case 0x15:
-        DELAY_TIMER = REG[regX];
+        case 0x0A:
+            //waits for key input
+            break;
+            
+        case 0x15:
+            DELAY_TIMER = REG[regX];
 
-    case 0x18:
-        SOUND_TIMER = REG[regX];
-        break;
+        case 0x18:
+            SOUND_TIMER = REG[regX];
+            break;
 
-    case 0x1E:
-        I += REG[regX];
-        break;
+        case 0x1E:
+            I += REG[regX];
+            break;
 
-    case 0x29:
-        //something silly
-        break;
+        case 0x29:
+            //something silly
+            break;
 
-    case 0x33:
-       break; 
+        case 0x33:
+        break; 
 
-    default:
-        break;
+        default:
+            break;
     }
 }
 
